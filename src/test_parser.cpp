44aa6d9f0849fc8abf07f48fe9e805677422f952
vladislav <vladnovoren@gmail.com>
2022-04-04 12:03:46 +0300

    warnings

--- src/test_parser.cpp
+++ src/test_parser.cpp
@@ -8,7 +8,8 @@ int main (int argc, char* argv[]) {
     }
 
     const char* dic_file_name = argv[1];
-    DicBuf dic = DicBuf_ParseDicFile(dic_file_name);
+    DicBuf dic;
+    DicBuf_ParseDicFile(&dic, dic_file_name);
     for (size_t elem_num = 0; elem_num < dic.n_elems; elem_num++) {
         printf("word: %s, value: %s\n", dic.elems[elem_num].key, dic.elems[elem_num].value);
     }
